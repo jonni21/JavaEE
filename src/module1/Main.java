09978547e3c004e22ad7198e5cb4028c93442124
jonni21 <jhnzvzd@gmail.com>
2016-04-04 13:44:52 +0300

    readme.md added

--- src/module1/Main.java
+++ src/module1/Main.java
@@ -5,13 +5,12 @@ import static module1.CollectionType.*;
 public class Main {
 
     public static void main(String[] args) {
-
-        ReportGenerator rp = new ReportGenerator();
-        rp.addRow(ARRAY_LIST);
-        rp.addRow(LINKED_LIST);
-        rp.addRow(HASH_SET);
-        rp.addRow(TREE_SET);
-        rp.saveToFile("report.txt");
-        rp.printTable();
+        ReportGenerator reportGenerator = new ReportGenerator();
+        reportGenerator.addRow(ARRAY_LIST);
+        reportGenerator.addRow(LINKED_LIST);
+        reportGenerator.addRow(HASH_SET);
+        reportGenerator.addRow(TREE_SET);
+        reportGenerator.saveToFile("readme.md");
+        reportGenerator.printTable();
     }
 }
